e20e71738850e34dfeb1307fc25bf743948c3cef
Karem <karemalani2402@yahoo.com>
2020-10-26 17:49:04 -0400

    activity and layout naming
    added background color for each fragment

--- app/src/main/java/abdulkarem/alani/s300993768/AbdulkaremMainActivity.java
+++ app/src/main/java/abdulkarem/alani/s300993768/AbdulkaremMainActivity.java
@@ -10,12 +10,12 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+public class AbdulkaremMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.abdulkarem_activity);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -26,5 +26,4 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
-
 }
