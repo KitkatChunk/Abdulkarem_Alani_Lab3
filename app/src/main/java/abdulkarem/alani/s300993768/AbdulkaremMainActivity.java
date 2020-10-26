3bdcb360c5dd071627ae8f2c7381b05226363f31
Karem <karemalani2402@yahoo.com>
2020-10-26 19:50:33 -0400

    Created draw and paintview

--- app/src/main/java/abdulkarem/alani/s300993768/AbdulkaremMainActivity.java
+++ app/src/main/java/abdulkarem/alani/s300993768/AbdulkaremMainActivity.java
@@ -2,18 +2,19 @@ package abdulkarem.alani.s300993768;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class AbdulkaremMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.abdulkarem_activity);
         BottomNavigationView navView = findViewById(R.id.nav_view);
@@ -26,4 +27,8 @@ public class AbdulkaremMainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
+
+
+
+
 }
